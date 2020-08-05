29d4d4d37d4639df0a0c8bc1157c5db9cb83943c
Neeraj Kumar <55330971+Ryuk-me@users.noreply.github.com>
2020-08-05 08:46:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def torrent(search):
     url = "https://1337x.to/search/"+search+"/1/"
 
 
-    hrome_options = webdriver.ChromeOptions()
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
