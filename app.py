2d740fa3db02d95e6ddcd7f00b727a92f6fc23e9
Neeraj Kumar <55330971+Ryuk-me@users.noreply.github.com>
2020-08-05 11:04:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 from flask import Flask ,jsonify
 import json
+import os
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -9,6 +11,7 @@ app.url_map.strict_slashes = False
 
 def torrent(search):
     url = "https://1337x.to/search/"+search+"/1/"
+
 
 
     res = requests.get(url,headers={
@@ -52,8 +55,30 @@ def torrent(search):
         if link_text.startswith("/torrent"):
             link_text = "https://1337x.to"+link_text
             link_list.append(link_text)
+    
+    
+    magnet_url_list = []
 
-    return file_name,file_size,link_list,date_list,seeder_list,leecher_list
+    for url_list in link_list:
+        magnet_url_list_append = []
+
+        magnet_req = requests.get(url_list,headers={
+                    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'})
+        with open("source.txt",'w') as file:
+            file.writelines(str(magnet_req.content))
+
+        with open('source.txt','r+') as f:
+            for line in f: 
+                # reading each word         
+                for word in line.split(): 
+                    if 'magnet' in word:
+                    # displaying the words            
+                        magnet_url_list_append.append(word)
+        magnet_url = magnet_url_list_append[0].split('"')[1]
+        magnet_url_list.append(magnet_url)
+        
+
+    return file_name,file_size,link_list,date_list,seeder_list,leecher_list,magnet_url_list
 
 @app.route('/')
 def home_page():
@@ -62,9 +87,9 @@ def home_page():
 @app.route('/<query>')
 def home(query):
 
-    file_name,file_size,link_list,date_list,seeder_list,leecher_list = torrent(query)
+    file_name,file_size,link_list,date_list,seeder_list,leecher_list,magnet_url_list = torrent(query)
 
-    return jsonify([{'Name':file_name[index],'size':file_size[index],'url':link_list[index],'date': date_list[index],'seeders':seeder_list[index],'leechers': leecher_list[index]} for index in range(len(file_name))])
+    return jsonify([{'Name':file_name[index],'size':file_size[index],'url':link_list[index],'date': date_list[index],'seeders':seeder_list[index],'leechers': leecher_list[index],'magnet': magnet_url_list[index]} for index in range(len(file_name))])
 
 
 
