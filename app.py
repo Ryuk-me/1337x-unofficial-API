b5153c83b113c25b3276af7c536fd099a826ef87
Neeraj Kumar <55330971+Ryuk-me@users.noreply.github.com>
2020-08-05 13:10:04 +0530

    Added Total downloads

--- app.py
+++ app.py
@@ -12,8 +12,6 @@ app.url_map.strict_slashes = False
 def torrent(search):
     url = "https://1337x.to/search/"+search+"/1/"
 
-
-
     res = requests.get(url,headers={
                     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'})
                 
@@ -27,6 +25,7 @@ def torrent(search):
     date_list = []
     seeder_list = []
     leecher_list =[]
+    
 
     for tr in table_rows:
         td = tr.find_all('td',{'class':["coll-1","name"]})
@@ -58,41 +57,59 @@ def torrent(search):
     
     
     magnet_url_list = []
+    total_downloads_list = []
+    last_checked_list = []
+    
 
     for url_list in link_list:
         magnet_url_list_append = []
 
         magnet_req = requests.get(url_list,headers={
                     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'})
+        new_soup = BeautifulSoup(magnet_req.content,'lxml')
+        everything_data_list = []
+        for span in new_soup.find_all('span'):
+            if span.text != "":
+                everything_data_list.append(span.text)
+
+        total_downloads_list.append(everything_data_list[6])
+        last_checked_list.append(everything_data_list[7])
+        
+        
         with open("source.txt",'w') as file:
             file.writelines(str(magnet_req.content))
 
         with open('source.txt','r+') as f:
-            for line in f: 
-                # reading each word         
+            for line in f:       
                 for word in line.split(): 
-                    if 'magnet' in word:
-                    # displaying the words            
+                    if 'magnet' in word:            
                         magnet_url_list_append.append(word)
         magnet_url = magnet_url_list_append[0].split('"')[1]
         magnet_url_list.append(magnet_url)
-        
-
-    return file_name,file_size,link_list,date_list,seeder_list,leecher_list,magnet_url_list
+    
+    return file_name,file_size,link_list,date_list,seeder_list,leecher_list,magnet_url_list,total_downloads_list,last_checked_list
 
 @app.route('/')
 def home_page():
-    return "Welcome"
+    return "Welcome To Unofficial Torrent API"
 
 @app.route('/<query>')
 def home(query):
 
-    file_name,file_size,link_list,date_list,seeder_list,leecher_list,magnet_url_list = torrent(query)
+    file_name,file_size,link_list,date_list,seeder_list,leecher_list,magnet_url_list,total_downloads_list,last_checked_list = torrent(query)
 
-    return jsonify([{'Name':file_name[index],'size':file_size[index],'url':link_list[index],'date': date_list[index],'seeders':seeder_list[index],'leechers': leecher_list[index],'magnet': magnet_url_list[index]} for index in range(len(file_name))])
+    return jsonify([{'Name':file_name[index],
+    'size':file_size[index],'url':link_list[index],
+    'uploadDate': date_list[index],
+    'seeders':seeder_list[index],
+    'leechers': leecher_list[index],
+    'magnet': magnet_url_list[index],
+    'downloads':total_downloads_list[index],
+    'lastChecked': last_checked_list[index]} 
+    for index in range(len(file_name))])
 
 
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host="0.0.0.0",port=5000)
+    app.run(port=5000)
