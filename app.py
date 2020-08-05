617c8fe19e162caa412d198da340dafd9aadf725
Neeraj Kumar <55330971+Ryuk-me@users.noreply.github.com>
2020-08-06 03:51:35 +0530

    Category && type added

--- app.py
+++ app.py
@@ -16,35 +16,7 @@ def torrent(search):
                     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'})
                 
     soup = BeautifulSoup(res.content,'lxml')
-
-    table = soup.find('table')
-    table_rows = table.find_all('tr')
-
-    file_name = []
-    file_size = []
-    date_list = []
-    seeder_list = []
-    leecher_list =[]
     
-
-    for tr in table_rows:
-        tp = tr.find_all('td',{'class':["size"]}) 
-        date_find = tr.find_all('td',{'class':["coll-date"]})
-        seeder_find =tr.find_all('td',{'class':["coll-2","seeds"]})
-        leecher_find =tr.find_all('td',{'class':["coll-3" ,"leeches"]})
-
-        for size,date,seeder,leecher in zip(tp,date_find,seeder_find,leecher_find):
-            date_list.append(date.text)
-            seeder_list.append(seeder.text)
-            leecher_list.append(leecher.text)
-            if "GB" in size.text:
-                size = size.text.split(' ')[0]+" GB"
-                file_size.append(size)
-            else:
-                size = size.text.split(' ')[0]+" MB"
-                file_size.append(size)
-
-
     link_list = []
 
     for a in soup.find_all('a', href=True, text=True):
@@ -53,10 +25,21 @@ def torrent(search):
             link_text = "https://1337x.to"+link_text
             link_list.append(link_text)
     
-    
+    file_name = []
     magnet_url_list = []
     total_downloads_list = []
     last_checked_list = []
+    total_downloads_list = []
+    last_checked_list = []
+    date_uploaded_list = []
+    file_size = []
+    date_uploaded_list = []
+    seeder_list = []
+    leecher_list =[]
+    language_list =[]
+    category_list = []
+    type_list = []
+
     
 
     for url_list in link_list:
@@ -65,6 +48,7 @@ def torrent(search):
         magnet_req = requests.get(url_list,headers={
                     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'})
         new_soup = BeautifulSoup(magnet_req.content,'lxml')
+        
         everything_data_list = []
 
         div = new_soup.find('div',class_='box-info-heading').text
@@ -73,8 +57,15 @@ def torrent(search):
             if span.text != "":
                 everything_data_list.append(span.text)
 
+        category_list.append(everything_data_list[1])
+        type_list.append(everything_data_list[2])
         total_downloads_list.append(everything_data_list[6])
         last_checked_list.append(everything_data_list[7])
+        date_uploaded_list.append(everything_data_list[8])
+        file_size.append(everything_data_list[4])
+        seeder_list.append(everything_data_list[9])
+        leecher_list.append(everything_data_list[10])
+        language_list.append(everything_data_list[3])
         
         
         with open("source.txt",'w') as file:
@@ -90,7 +81,7 @@ def torrent(search):
         magnet_url = magnet_url_list_append[0].split('"')[1]
         magnet_url_list.append(magnet_url)
     
-    return file_name,file_size,link_list,date_list,seeder_list,leecher_list,magnet_url_list,total_downloads_list,last_checked_list
+    return file_name,file_size,link_list,date_uploaded_list,seeder_list,leecher_list,magnet_url_list,total_downloads_list,last_checked_list,language_list,type_list,category_list
 
 @app.route('/')
 def home_page():
@@ -99,20 +90,23 @@ def home_page():
 @app.route('/<query>')
 def home(query):
 
-    file_name,file_size,link_list,date_list,seeder_list,leecher_list,magnet_url_list,total_downloads_list,last_checked_list = torrent(query)
+    file_name,file_size,link_list,date_uploaded_list,seeder_list,leecher_list,magnet_url_list,total_downloads_list,last_checked_list,language_list,category_list,type_list = torrent(query)
 
     return jsonify([{'Name':file_name[index],
     'size':file_size[index],'url':link_list[index],
-    'uploadDate': date_list[index],
+    'uploadDate': date_uploaded_list[index],
     'seeders':seeder_list[index],
     'leechers': leecher_list[index],
     'magnet': magnet_url_list[index],
     'downloads':total_downloads_list[index],
-    'lastChecked': last_checked_list[index]} 
+    'lastChecked': last_checked_list[index],
+    'language': language_list[index],
+    'category': category_list[index],
+    'type': type_list[index]} 
     for index in range(len(file_name))])
 
 
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host"0.0.0.0",port=5000)
+    app.run(port=5000)
