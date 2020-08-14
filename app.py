8582275dcc5a720cd0f25bef863eda676cfa21c3
Neeraj Kumar <55330971+Ryuk-me@users.noreply.github.com>
2020-08-15 00:51:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -108,6 +108,3 @@ def home(query):
 if __name__ == "__main__":
     app.debug = True
     app.run(port=5000)
-# if __name__ == "__main__":
-#     app.debug = True
-#     app.run(host="0.0.0.0",port=5000)
