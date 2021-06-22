0f5804084e699f01d3522c9f21d3c641f23b97da
Neeraj Kumar <55330971+Ryuk-me@users.noreply.github.com>
2021-06-22 09:58:33 +0530

    changed site to 1337xx.to

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.url_map.strict_slashes = False
 
 
 def torrent(search):
-    url = "https://1337x.to/search/"+search+"/1/"
+    url = "https://1337xx.to/search/"+search+"/1/"
 
     res = requests.get(url,headers={
                     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'})
